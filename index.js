bb6098e6362893bf9802b0b39a8cec84f01c5d98
wgcairui <wgcairui@iclou.com>
2019-08-11 22:23:05 +0800

    08-12

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
  * @Description: In User Settings Edit
  * @Author: cairui
  * @Date: 2019-08-11 00:03:40
- * @LastEditTime: 2019-08-11 20:10:26
+ * @LastEditTime: 2019-08-11 22:20:51
  * @LastEditors: Please set LastEditors
  */
 /* jshint esversion:8 */
 const MongoClient = require('mongodb').MongoClient;
+const util = require('util')
 
 
 /**
@@ -50,28 +51,29 @@ class mongodb {
      * @param {*} { collection, msg }collection集合名称，msg为log消息体，
      * @memberof mongodb
      */
-    async write_log({ collection, msg: { infoType, infoStatu, infoBody, infoUser } }) {
-        if (await this.log && collection) {
+    async write_log({ log_collection = 'other', msg }) {
+        if (await this.log && log_collection) {
             try {
-                (await this.log).collection(collection).insertOne(Object.assign(msg,{generateTime:this.Dates().cn},{DateTime:new Date()}))
+                (await this.log).collection(log_collection).insertOne(Object.assign(msg, { generateTime: this.Dates().cn }, { DateTime: new Date() }))
             } catch (error) {
+                console.error(error);
 
             }
         }
     }
 
-    static Dates() {
+    Dates() {
         let d = new Date()
         let en = new Date(...[d.getFullYear(), d.getMonth() + 1, d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds()])
         let cn = new Date(...[d.getFullYear(), d.getMonth() + 1, d.getDate(), d.getHours() + 8, d.getMinutes(), d.getSeconds(), d.getMilliseconds()])
-        return {cn,en}
+        return { cn, en }
     }
 
     /**
      *
      *
-     * @param {*} [connect={ db, collection, }] 连接对象
-     * @param {*} [querys={ query: {}, result: {}, multi }] 查询对象
+     * @param {*} [connect={ db, collection, }] db数据库，collection集合
+     * @param {*} [querys={ query: {}, result: {}, multi }] query查询条件，result返回刷选，multi多选
      * @param {string} [log={ log_collection, msg: {}, user: 'all' }] 日志对象
      * @returns
      * @memberof mongodb
@@ -80,17 +82,75 @@ class mongodb {
         if (connect) {
             let { db, collection, } = connect;
             let { query, result, multi } = querys
+            let { log_collection, msg, user } = log
             try {
                 let Arr_result = (await this.Conection).db(db).collection(collection).find(query).project(Object.assign({ _id: 0 }, result)).toArray()
-                this.write_log({log_collection,msg:{infoType:'mongoFind',infoStatu:'success',infoBody:Object.assign(connect,querys),infoUser:user}})
+                console.log(log_collection);
+
+                this.write_log({ log_collection, msg: { infoType: 'mongoFind', infoStatu: 'success', infoBody: Object.assign(connect, querys), infoUser: user } })
                 return multi ? Arr_result : Arr_result[0]
             } catch (error) {
                 console.error(error)
-                this.write_log({log_collection,msg:{infoType:'mongoFind',infoStatu:'error',infoBody:Object.assign(connect,querys,{error:error}),infoUser:user}})
+                this.write_log({ log_collection, msg: { infoType: 'mongoFind', infoStatu: 'error', infoBody: Object.assign(connect, querys, { error: error }), infoUser: user } })
                 return false
             }
         } else {
             throw Error('find db arg is null')
+        }
+    }
+
+    /**
+     *
+     *
+     * @param {*} [connect={ db, collection, }]
+     * @param {*} [querys={ query}]
+     * @param {string} [log={ log_collection, msg: {}, user: 'all' }]
+     * @returns
+     * @memberof mongodb
+     */
+    async insert(connect = { db, collection, }, querys = { query }, log = { log_collection, msg: {}, user: 'all' }) {
+        if (connect) {
+            let { db, collection, } = connect;
+            let { query } = querys
+            let { log_collection, msg, user } = log
+            try {
+                let Arr_result
+                if (util.isArray(query)) {
+                    Arr_result = await (await this.Conection).db(db).collection(collection).insertMany(query)
+                } else {
+                    Arr_result = await (await this.Conection).db(db).collection(collection).insertOne(query)
+                }
+                this.write_log({ log_collection, msg: { infoType: 'mongoInsert', infoStatu: 'success', infoBody: Object.assign(connect, querys), infoUser: user } })
+                let { insertedCount, insertedId, insertedIds } = Arr_result
+                return { insertedCount, insertedId, insertedIds }
+            } catch (error) {
+                console.error(error)
+                this.write_log({ log_collection, msg: { infoType: 'mongoInsert', infoStatu: 'error', infoBody: Object.assign(connect, querys, { error: error }), infoUser: user } })
+                return false
+            }
+        } else {
+            throw Error('insert db arg is null')
+        }
+    }
+
+    /**
+     *
+     *因为处理不了原子操作符，顾updatSerize只处理 promise update
+     * @param {string} [log={ log_collection, msg: {}, user: 'all' }]
+     * @returns
+     * @memberof mongodb
+     */
+    async updatSerize(update_result, log = { log_collection, msg: {}, user: 'all' }) {
+        let { log_collection, msg, user } = log
+        try {
+            await update_result
+            this.write_log({ log_collection, msg: { infoType: 'mongoUpdate', infoStatu: 'success', infoBody: update_result, infoUser: user } })
+            let { result } = await update_result            
+            return result
+        } catch (error) {
+            console.error(error)
+            this.write_log({ log_collection, msg: { infoType: 'mongoUpdate', infoStatu: 'error', infoBody: Object.assign(await update_result, { error: error }), infoUser: user } })
+            return false
         }
     }
 
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-11 09:15:53
- * @LastEditTime: 2019-08-11 10:05:08
+ * @LastEditTime: 2019-08-11 22:19:24
  * @LastEditors: Please set LastEditors
  */
 /* jshint esversion:8 */
@@ -10,7 +10,12 @@ const mongodb = require('./index')
 const db = new mongodb(null,{db:'log',enable:true})
 
 const test = async ()=>{
-    let r = await db.find({db:'BS',collection:'bulletin'},{multi:true})
+    //let r = await db.find({db:'BS',collection:'bulletin'},{multi:true},{log_collection:'dev',user:'admin'})
+    //let r = await db.insert({db:'BS',collection:'bulletin'},{query:{date:'1234'}},{log_collection:'dev',user:'admin'})
+    //let r = await db.insert({db:'BS',collection:'bulletin'},{query:[{date:'1234'},{tes:'kjklm'}]},{log_collection:'dev',user:'admin'})
+    
+    let r =  (await db.Conection).db('BS').collection('bulletin').updateOne({date:'1234'},{$set:{update:1}},{upsert:true}) // db.update({db:'BS',collection:'bulletin'},{query:{date:'1234'},modify:{$set:{update:1}}},{log_collection:'dev',user:'admin'})
+    r = await db.updatSerize(r,{log_collection:'dev',user:'admin'})
     console.log(r)
 }
 
